dff9e9124e9eaac855a5f2ca14f227479c5e1198
23CSBNISHANTHR <139440974+23CSBNISHANTHR@users.noreply.github.com>
2023-08-22 09:59:23 +0000

    completed

---
+++ voting.java
@@ -0,0 +1,10 @@
+class voting{
+public static void main(String args[]){
+    int a=17;
+    if(a<=18){
+        System.out.println("Not Eligible");
+    }
+    else{
+        System.out.println(" Eligible"); 
+    }
+}}
